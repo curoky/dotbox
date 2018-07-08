1e338a58e386fe40f5af7de3cae44df3ff200bae
curoky <cccuroky@gmail.com>
2018-07-08 23:27:31 +0800

    Added: bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+cd "$(dirname $0)"
+
+export PATH=/usr/local/opt/openssl@3/bin:/Users/curoky/Library/Python/3.8/bin:$PATH
+
+if ! command -v dotdrop &>/dev/null; then
+  python3 -m pip install git+https://github.com/deadc0de6/dotdrop
+fi
+
+if [[ $(uname -s) == "Darwin" ]]; then
+  dotdrop install --force --profile=macos
+else
+  dotdrop install --force --profile=${1:-devbox}
+fi
