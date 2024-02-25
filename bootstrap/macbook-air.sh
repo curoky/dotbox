b8451f048f6eb392a2223a6dcedb1e2a71a98e99
curoky <cccuroky@gmail.com>
2024-02-25 23:55:54 +0800

    feat(bootstrap): support macos

---
+++ bootstrap/macbook-air.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install dotdrop
+
+dotdrop install --cfg=~/repos/dotbox/config.yaml --force --profile=macos-userconf
+
+brew bundle --force --file ~/repos/dotbox/images/config/brew/Brewfile.osx-persional --cleanup --verbose
