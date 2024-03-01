6c605baeb97fe8945271a70c514366b256b21ae7
curoky <cccuroky@gmail.com>
2024-03-02 02:22:07 +0800

    feat(images): support custom sshd port

---
+++ dists/docker/script/setup-sshd-port.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+listen_port=${1:-61001}
+sed -i -e "s/#Port 22/Port ${listen_port}/g" /etc/ssh/sshd_config
+sed -i -e "s/61000/${listen_port}/g" \
+  /etc/ssh/sshd_config.d/custom.conf \
+  /etc/systemd/system/ssh.socket.d/custom.conf
