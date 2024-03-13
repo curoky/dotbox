4a6f1e8a3c051a84b13bbfaa02188d6fb0b7e46b
curoky <cccuroky@gmail.com>
2024-03-13 15:31:00 +0800

    feat(images): migrate to nixpkg's sshd

--- dists/docker/script/setup-sshd-port.sh
+++ dists/docker/script/setup-sshd-port.sh
@@ -3,7 +3,9 @@
 set -xeuo pipefail
 
 listen_port=${1:-61001}
-sed -i -e "s/#Port 22/Port ${listen_port}/g" /etc/ssh/sshd_config
-sed -i -e "s/61000/${listen_port}/g" \
-  /etc/ssh/sshd_config.d/custom.conf \
-  /etc/systemd/system/ssh.socket.d/custom.conf
+# sed -i -e "s/#Port 22/Port ${listen_port}/g" /etc/ssh/sshd_config
+# sed -i -e "s/61000/${listen_port}/g" \
+#   /etc/ssh/sshd_config.d/custom.conf \
+#   /etc/systemd/system/ssh.socket.d/custom.conf
+
+sed -i -e "s/#Port 22/Port ${listen_port}/g" /o/dotbox/config/sshd/sshd_config.conf
