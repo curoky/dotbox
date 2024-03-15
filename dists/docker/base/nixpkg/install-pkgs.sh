4c3c87ef8a42e4528f3c02d8242419f1a984cda7
curoky <cccuroky@gmail.com>
2024-03-15 10:23:25 +0800

    feat(images): update base images and lock files

--- dists/docker/base/nixpkg/install-pkgs.sh
+++ dists/docker/base/nixpkg/install-pkgs.sh
@@ -22,18 +22,18 @@ pkg_list=(
   # system core package
   nixpkgs.tzdata.out nixpkgs.glibcLocales nixpkgs.locale
   # compression
-  nixpkgs.bzip2 nixpkgs.xz nixpkgs.zstd nixpkgs.zip nixpkgs.unzip nixpkgs.gnutar nixpkgs.zlib nixpkgs.gzip nixpkgs.snappy
+  nixpkgs.bzip2 nixpkgs.xz nixpkgs.zstd nixpkgs.zip nixpkgs.unzip nixpkgs.gnutar nixpkgs.zlib nixpkgs.zlib-ng nixpkgs.gzip nixpkgs.snappy
   # download
   nixpkgs.gnupg nixpkgs.cacert nixpkgs.curl nixpkgs.wget nixpkgs.aria2 nixpkgs.rsync
   # edit
-  nixpkgs.less nixpkgs.gnused nixpkgs.gnugrep nixpkgs.vim nixpkgs.bat nixpkgs.diffutils nixpkgs.jq
+  nixpkgs.less nixpkgs.gnused nixpkgs.gnugrep nixpkgs.vim nixpkgs.bat nixpkgs.jq
   nixpkgs.flex nixpkgs.bison nixpkgs.gettext nixpkgs.m4
   # SRE
   nixpkgs.iproute2 nixpkgs.iputils nixpkgs.netcat nixpkgs.lsof nixpkgs.connect nixpkgs.procps
   nixpkgs.nettools nixpkgs.fd nixpkgs.ethtool nixpkgs.gdb nixpkgs.file nixpkgs.eza nixpkgs.fzf
   nixpkgs.dstat nixpkgs.man nixpkgs.iptables nixpkgs.unixtools.xxd nixpkgs.util-linux
   nixpkgs.findutils nixpkgs.coreutils nixpkgs.cron nixpkgs.lsb-release nixpkgs.tree
-  nixpkgs.gdu nixpkgs.ncdu nixpkgs.silver-searcher nixpkgs.sqlite
+  nixpkgs.gdu nixpkgs.ncdu nixpkgs.silver-searcher nixpkgs.diffutils
   # libraries
   nixpkgs.ncurses5 nixpkgs.libxml2.out nixpkgs.libxcrypt nixpkgs.libbacktrace nixpkgs.jemalloc nixpkgs.flamegraph
   # c/c++
@@ -59,7 +59,7 @@ pkg_list=(
   nixpkgs.git nixpkgs.git-absorb nixpkgs.git-extras nixpkgs.git-lfs nixpkgs.git-filter-repo nixpkgs.gnupatch
   # other tools
   nixpkgs.pre-commit nixpkgs.gperf nixpkgs.openssh nixpkgs.pandoc nixpkgs.graphviz
-  nixpkgs.parallel nixpkgs.cloc nixpkgs.go-task nixpkgs.krb5
+  nixpkgs.parallel nixpkgs.cloc nixpkgs.go-task nixpkgs.krb5 nixpkgs.sqlite
 
   # nixpkgs.gost nixpkgs.htop nixpkgs.tcpdump nixpkgs.shellcheck
   # nixpkgs.mkdocs nixpkgs.sphinx nixpkgs.hugo nixpkgs.inetutils
