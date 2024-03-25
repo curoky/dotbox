fb457812fa1f203330d342f6a238d186942fcbcb
curoky <cccuroky@gmail.com>
2024-03-25 17:53:27 +0800

    feat(images): fix some details

--- dists/docker/lock/lock.sh
+++ dists/docker/lock/lock.sh
@@ -3,6 +3,6 @@
 root=$(date +%Y-%m-%d)
 mkdir -p $root
 apt list --installed >$root/apt-list.txt
-conda env export -n tf2.15 >$root/conda-tf2.15.yaml
+conda env export -n tf2.15 >$root/conda-tf2.15-cpu.yaml
 conda env export -n py3 >$root/conda-py3.yaml
 conda env export -n py2 >$root/conda-py2.yaml
