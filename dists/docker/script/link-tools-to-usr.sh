9bcce50c5648e180cd6ef84d47a14b357a2368ad
curoky <cccuroky@gmail.com>
2024-05-07 15:23:38 +0800

    fix(bazel): update link tools script

--- dists/docker/script/link-tools-to-usr.sh
+++ dists/docker/script/link-tools-to-usr.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -xeuo pipefail
 
-# link nixpkg bin and lib
 BIN_NAMES=(curl grep sed gzip find less git) # perl bash
 for n in "${BIN_NAMES[@]}"; do
-  [[ ! -e /bin/$n ]] && ln -s /nix/var/nix/profiles/default/bin/$n /bin/$n
-  [[ ! -e /usr/bin/$n ]] && ln -s /nix/var/nix/profiles/default/bin/$n /usr/bin/$n
+  if [[ ! -e /usr/bin/$n ]]; then
+    ln -s /nix/var/nix/profiles/default/bin/$n /usr/bin/$n
+  fi
 done
