38af72c3ac5b0825c42d7bdf627c85ab42efba48
curoky <cccuroky@gmail.com>
2024-05-16 11:48:23 +0800

    feat(dist/docker): remove /app/pipx/bin from path

--- dists/docker/script/link-tools-to-usr.sh
+++ dists/docker/script/link-tools-to-usr.sh
@@ -7,3 +7,6 @@ for n in "${BIN_NAMES[@]}"; do
     ln -s /nix/var/nix/profiles/default/bin/$n /usr/bin/$n
   fi
 done
+
+ln -s /app/pipx/bin/{dotdrop,netron,licenseheaders} /usr/local/bin
+ln -s /nix/var/nix/profiles/default/bin/{curl,grep,sed,gzip,find,less,git,zsh} /usr/local/bin
