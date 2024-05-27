5ac2f420d59136a7cd3f10c11be714d1d9c0f30e
curoky <cccuroky@gmail.com>
2024-05-27 17:26:11 +0800

    feat(dist/docker): add tf2.15 and update script

--- dists/docker/script/setup-sys-change-default-gcc.sh
+++ dists/docker/script/setup-sys-change-default-gcc.sh
@@ -17,10 +17,11 @@
 # limitations under the License.
 
 target_gcc_version=${1:-13}
+priority=${2:-30}
 
 if [[ -f /usr/bin/gcc-$target_gcc_version ]]; then
   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$target_gcc_version \
-    20 \
+    $priority \
     --slave /usr/bin/gcov gcov /usr/bin/gcov-$target_gcc_version \
     --slave /usr/bin/gcov-dump gcov-dump /usr/bin/gcov-dump-$target_gcc_version \
     --slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-$target_gcc_version \
@@ -28,7 +29,7 @@ if [[ -f /usr/bin/gcc-$target_gcc_version ]]; then
     --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-$target_gcc_version \
     --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-$target_gcc_version
 
-  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$target_gcc_version 20
-  update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-$target_gcc_version 20
-  update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-$target_gcc_version 20
+  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$target_gcc_version $priority
+  update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-$target_gcc_version $priority
+  update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-$target_gcc_version $priority
 fi
